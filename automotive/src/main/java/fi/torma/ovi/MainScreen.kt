f75ccf22387c6ab4c0f78f7b96a351c9f8f44bed
jjtt <3908945+jjtt@users.noreply.github.com>
2024-02-03 18:31:26 +0200

    A button

--- automotive/src/main/java/fi/torma/ovi/MainScreen.kt
+++ automotive/src/main/java/fi/torma/ovi/MainScreen.kt
@@ -1,23 +1,37 @@
 package fi.torma.ovi
 
 import androidx.car.app.CarContext
+import androidx.car.app.CarToast
 import androidx.car.app.Screen
 import androidx.car.app.model.Action
+import androidx.car.app.model.ActionStrip
 import androidx.car.app.model.Pane
 import androidx.car.app.model.PaneTemplate
 import androidx.car.app.model.Row
 import androidx.car.app.model.Template
 
 
-class MainScreen(carContext: CarContext): Screen(carContext) {
+class MainScreen(carContext: CarContext) : Screen(carContext) {
     override fun onGetTemplate(): Template {
         val row: Row = Row.Builder()
             .setTitle("Terve, #badgamers").build()
+
+        val toast: () -> Unit = {
+            CarToast.makeText(carContext, "Moi!", CarToast.LENGTH_SHORT).show()
+        }
+        val action = Action.Builder().setOnClickListener(toast)
+            .setTitle("Sano moi")
+            .build()
+
+        val actionStrip: ActionStrip = ActionStrip.Builder().addAction(action).build()
 
         return PaneTemplate.Builder(
             Pane.Builder()
                 .addRow(row)
                 .build()
-        ).setHeaderAction(Action.APP_ICON).build()
+        )
+            .setActionStrip(actionStrip)
+            .setHeaderAction(Action.APP_ICON)
+            .build()
     }
 }
