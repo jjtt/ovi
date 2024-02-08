b6b09da275d9680e8d80eeddfedb362817002329
jjtt <3908945+jjtt@users.noreply.github.com>
2024-02-08 21:46:38 +0200

    Actual Shelly requests and a second screen

--- automotive/src/main/java/fi/torma/ovi/SettingsScreen.kt
+++ automotive/src/main/java/fi/torma/ovi/SettingsScreen.kt
@@ -1,36 +1,25 @@
 package fi.torma.ovi
 
 import androidx.car.app.CarContext
-import androidx.car.app.CarToast
 import androidx.car.app.Screen
 import androidx.car.app.model.Action
-import androidx.car.app.model.ActionStrip
 import androidx.car.app.model.Pane
 import androidx.car.app.model.PaneTemplate
 import androidx.car.app.model.Row
 import androidx.car.app.model.Template
 
-
-class MainScreen(carContext: CarContext) : Screen(carContext) {
+class SettingsScreen(carContext: CarContext) : Screen(carContext) {
     override fun onGetTemplate(): Template {
         val row: Row = Row.Builder()
-            .setTitle("Terve, #badgamers").build()
-
-        val action = Action.Builder().setOnClickListener{
-            screenManager.push(SettingsScreen(carContext))
-        }
-            .setTitle("Settings")
-            .build()
-
-        val actionStrip: ActionStrip = ActionStrip.Builder().addAction(action).build()
+            .setTitle("Welcome to the new screen!").build()
 
         return PaneTemplate.Builder(
             Pane.Builder()
                 .addRow(row)
                 .build()
         )
-            .setActionStrip(actionStrip)
             .setHeaderAction(Action.APP_ICON)
             .build()
     }
 }
+
