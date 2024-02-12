f7b0ed35058fd1f195916f469b6b1f0d73173e7a
jjtt <3908945+jjtt@users.noreply.github.com>
2024-02-12 21:15:08 +0200

    Use location

--- automotive/src/main/java/fi/torma/ovi/MainScreen.kt
+++ automotive/src/main/java/fi/torma/ovi/MainScreen.kt
@@ -1,6 +1,11 @@
 package fi.torma.ovi
 
 import SettingsScreen
+import android.content.Context
+import android.content.pm.PackageManager
+import android.location.Location
+import android.location.LocationListener
+import android.location.LocationManager
 import androidx.car.app.CarContext
 import androidx.car.app.CarToast
 import androidx.car.app.Screen
@@ -11,6 +16,7 @@ import androidx.car.app.model.GridItem
 import androidx.car.app.model.GridTemplate
 import androidx.car.app.model.ItemList
 import androidx.car.app.model.Template
+import androidx.core.content.ContextCompat
 import androidx.core.graphics.drawable.IconCompat
 import androidx.lifecycle.DefaultLifecycleObserver
 import androidx.lifecycle.LifecycleOwner
@@ -21,6 +27,59 @@ import password
 
 class MainScreen(carContext: CarContext) : Screen(carContext), DefaultLifecycleObserver {
     private var inputStatus: String? = null
+    private var closeToDoor: Boolean? = false
+    private var locationManager: LocationManager? = null
+    private var locationListener: LocationListener? = null
+
+    override fun onCreate(owner: LifecycleOwner) {
+        super.onCreate(owner)
+
+
+        locationManager = carContext.getSystemService(Context.LOCATION_SERVICE) as LocationManager
+
+        locationListener = LocationListener { location ->
+            val targetLocation = Location("target").apply {
+                latitude = 60.0
+                longitude = 25.0
+            }
+            val distance = location.distanceTo(targetLocation)
+            closeToDoor = distance < 30
+        }
+
+        if (ContextCompat.checkSelfPermission(
+                carContext,
+                android.Manifest.permission.ACCESS_FINE_LOCATION
+            )
+            != PackageManager.PERMISSION_GRANTED
+        ) {
+            // Permission is not granted
+            // Request the permission
+            CarToast.makeText(carContext, "Requesting location permission", CarToast.LENGTH_LONG)
+                .show()
+            carContext.requestPermissions(
+                listOf(
+                    android.Manifest.permission.ACCESS_FINE_LOCATION,
+                    android.Manifest.permission.ACCESS_COARSE_LOCATION
+                )
+            ) { grantedPermissions, _ ->
+                if (grantedPermissions.contains(android.Manifest.permission.ACCESS_FINE_LOCATION)) {
+                    requestLocation()
+                }
+            }
+        } else {
+            requestLocation()
+        }
+    }
+
+    private fun requestLocation() {
+        locationManager?.requestLocationUpdates(
+            LocationManager.GPS_PROVIDER,
+            10000L,
+            10f,
+            locationListener!!
+        )
+    }
+
     override fun onStop(owner: LifecycleOwner) {
         super.onStop(owner)
         inputStatus = null
@@ -95,10 +154,19 @@ class MainScreen(carContext: CarContext) : Screen(carContext), DefaultLifecycleO
     }
 
     private fun toggleDoor() {
-        GlobalScope.launch {
-            requestSwitchOn(password(carContext))
-            inputStatus = null
-            invalidate()
+        if (closeToDoor == true) {
+            GlobalScope.launch {
+                requestSwitchOn(password(carContext))
+                inputStatus = null
+                invalidate()
+            }
+        } else {
+            CarToast.makeText(
+                carContext,
+                "You are not close enough to the door",
+                CarToast.LENGTH_LONG
+            )
+                .show()
         }
     }
 }
